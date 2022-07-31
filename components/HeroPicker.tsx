6c5e43c8b55f13d5967eb2660e121ee2439ec846
yuval1121 <yuvalvarshevsky@gmail.com>
2022-07-31 12:37:24 +0300

    refactored heropicker

--- components/HeroPicker.tsx
+++ components/HeroPicker.tsx
@@ -3,6 +3,7 @@ import { ChangeEvent, useState } from 'react';
 import { Hero } from '../types';
 import HeroCard from './HeroCard';
 import sample from 'lodash.sample';
+import RolePicker from './RolePicker';
 
 type Props = {
   heroes: Hero[];
@@ -10,9 +11,6 @@ type Props = {
 
 const HeroPicker = ({ heroes }: Props) => {
   const [isShown, setIsShown] = useState(false);
-  const [isTank, setIsTank] = useState(true);
-  const [isDps, setIsDps] = useState(true);
-  const [isSupport, setIsSupport] = useState(true);
   const [hero, setHero] = useState<Hero>(heroes[0]);
   const [heroPool, setHeroPool] = useState<Hero[]>(heroes);
 
@@ -24,54 +22,16 @@ const HeroPicker = ({ heroes }: Props) => {
     if (hero) setHero(hero);
   };
 
-  const tankClickHandler = (e: ChangeEvent<HTMLInputElement>) => {
-    if (e.currentTarget.checked) {
-      setHeroPool(heroPool.concat(heroes.filter(e => e.role === 'tank')));
-    } else {
-      setHeroPool(heroPool.filter(e => e.role !== 'tank'));
-    }
-    setIsTank(e.currentTarget.checked);
-  };
-
-  const dpsClickHandler = (e: ChangeEvent<HTMLInputElement>) => {
-    if (e.currentTarget.checked) {
-      setHeroPool(heroPool.concat(heroes.filter(e => e.role === 'dps')));
-    } else {
-      setHeroPool(heroPool.filter(e => e.role !== 'dps'));
-    }
-    setIsDps(e.currentTarget.checked);
-  };
-
-  const supportClickHandler = (e: ChangeEvent<HTMLInputElement>) => {
-    if (e.currentTarget.checked) {
-      setHeroPool(heroPool.concat(heroes.filter(e => e.role === 'support')));
-    } else {
-      setHeroPool(heroPool.filter(e => e.role !== 'support'));
-    }
-    setIsSupport(e.currentTarget.checked);
-  };
-
   return (
     <>
       <div style={{ display: 'flex', flexDirection: 'column' }}>
         <Button onClick={handleClick}>Hero Randomizer</Button>
       </div>
-      <div
-        style={{
-          display: 'flex',
-          justifyContent: 'space-between',
-          flexWrap: 'wrap',
-          marginTop: '1rem',
-        }}
-      >
-        <Checkbox checked={isTank} onChange={tankClickHandler} label="Tank" />
-        <Checkbox checked={isDps} onChange={dpsClickHandler} label="DPS" />
-        <Checkbox
-          checked={isSupport}
-          onChange={supportClickHandler}
-          label="Support"
-        />
-      </div>
+      <RolePicker
+        heroes={heroes}
+        heroPool={heroPool}
+        setHeroPool={setHeroPool}
+      />
       {isShown && <HeroCard hero={hero} />}
     </>
   );
---
+++ components/RolePicker.tsx
@@ -0,0 +1,63 @@
+import { Checkbox } from '@mantine/core';
+import { ChangeEvent, Dispatch, SetStateAction, useState } from 'react';
+import { Hero } from '../types';
+
+type Props = {
+  heroes: Hero[];
+  heroPool: Hero[];
+  setHeroPool: Dispatch<SetStateAction<Hero[]>>;
+};
+
+const RolePicker = ({ heroes, heroPool, setHeroPool }: Props) => {
+  const [isTank, setIsTank] = useState(true);
+  const [isDps, setIsDps] = useState(true);
+  const [isSupport, setIsSupport] = useState(true);
+
+  const tankClickHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.currentTarget.checked) {
+      setHeroPool(heroPool.concat(heroes.filter(e => e.role === 'tank')));
+    } else {
+      setHeroPool(heroPool.filter(e => e.role !== 'tank'));
+    }
+    setIsTank(e.currentTarget.checked);
+  };
+
+  const dpsClickHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.currentTarget.checked) {
+      setHeroPool(heroPool.concat(heroes.filter(e => e.role === 'dps')));
+    } else {
+      setHeroPool(heroPool.filter(e => e.role !== 'dps'));
+    }
+    setIsDps(e.currentTarget.checked);
+  };
+
+  const supportClickHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.currentTarget.checked) {
+      setHeroPool(heroPool.concat(heroes.filter(e => e.role === 'support')));
+    } else {
+      setHeroPool(heroPool.filter(e => e.role !== 'support'));
+    }
+    setIsSupport(e.currentTarget.checked);
+  };
+
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'space-between',
+        flexWrap: 'wrap',
+        marginTop: '1rem',
+      }}
+    >
+      <Checkbox checked={isTank} onChange={tankClickHandler} label="Tank" />
+      <Checkbox checked={isDps} onChange={dpsClickHandler} label="DPS" />
+      <Checkbox
+        checked={isSupport}
+        onChange={supportClickHandler}
+        label="Support"
+      />
+    </div>
+  );
+};
+
+export default RolePicker;
